f9e60a8912887a759dfded857ebf9c9cd0149cc3
felixojiambo <felixojiamboe@gmail.com>
2024-12-20 17:09:39 +0300

    add RouterModule to imports in the @Component decorator of SidenavComponent

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Output, EventEmitter } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css'],
 })
