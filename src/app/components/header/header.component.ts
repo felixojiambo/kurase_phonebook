b2b677b2d2468860a0611922b1d24890938d94e9
felixojiambo <felixojiamboe@gmail.com>
2024-12-20 19:32:51 +0300

    minor changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,31 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component, Output, EventEmitter, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [CommonModule, FormsModule], 
+  imports:[CommonModule,FormsModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
- 
 })
 export class HeaderComponent {
+  @Output() toggleSidenav = new EventEmitter<void>();
   @Output() toggleView = new EventEmitter<void>();
   @Output() toggleDarkMode = new EventEmitter<void>();
   @Output() search = new EventEmitter<string>();
 
   searchTerm: string = '';
+  viewMode: 'list' | 'grid' = 'list';
+  isSidenavOpen: boolean = false;
 
-  onSearchChange() {
-    this.search.emit(this.searchTerm);
+  onToggleSidenav() {
+    this.isSidenavOpen = !this.isSidenavOpen;
+    this.toggleSidenav.emit();
   }
 
   onToggleView() {
+    this.viewMode = this.viewMode === 'list' ? 'grid' : 'list';
     this.toggleView.emit();
   }
 
   onToggleDarkMode() {
     this.toggleDarkMode.emit();
+  }
+
+  onSearchChange() {
+    this.search.emit(this.searchTerm);
   }
 }
